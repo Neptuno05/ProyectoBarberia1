7bd343b34cf829f762df113b75421c28ca9178bb
jo.gallardoj@duocuc.cl <jo.gallardoj@duocuc.cl>
2024-09-03 23:49:28 -0400

    primera etapa

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProyectoBarberia',
+  webDir: 'www'
+};
+
+export default config;
