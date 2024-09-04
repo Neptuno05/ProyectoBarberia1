b9bc6e85e12f62c3e5156a477394dc02a2ce1ee4
Samuel Mathias <sa.mathias@duocuc.cl>
2024-09-04 01:31:25 -0400

    Login y Home con boton Atras Listo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
+  goBack(){
+    this.navCtrl.back();
+  }
   ngOnInit() {
   }
 
