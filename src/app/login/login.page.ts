8d5d22d3b3d57005e50b68258bc386845f901fc7
Samuel Mathias <sa.mathias@duocuc.cl>
2024-09-04 01:20:35 -0400

    Login con boton Atras

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
+  goBack(){
+    this.navCtrl.back();
+  }
   ngOnInit() {
   }
 
